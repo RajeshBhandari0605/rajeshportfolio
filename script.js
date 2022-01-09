fd5b5ca0fe1dfe95f4fd4811aa36a1a62edbd3e2
RajeshBhandari0605 <rajeshbhandari0605@gmail.com>
2022-01-09 20:54:08 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY >20){
+            $('.navbar') .addClass("sticky");
+
+        }else{
+            $('.navbar') .removeClass("sticky");
+    }
+
+});
+
+//toggle meanu/navbar script
+$('.menu-btn') .Click(function(){
+    $('.navbar.menu') .toggleClass("active");
+    $('.menu-btn i') .toggleClass("active");
+
+});
+});
